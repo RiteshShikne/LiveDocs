d0beed724d3a7d5cc0f3a7b55ce325a01824a0cc
RITESH <riteshshikne@gmail.com>
2024-07-28 20:10:25 +0530

    Seconf commit

---
+++ lib/liveblocks.ts
@@ -0,0 +1,5 @@
+import { Liveblocks } from "@liveblocks/node";
+
+export const liveblocks = new Liveblocks({
+  secret: "sk_prod_ooANEJzG7wT7UDIPwVpGUDKPAZ7C3azqcq204LV8ReHd_cbuBQ5fXyIrG4NpTiRi", // Replace with your actual secret key
+});
